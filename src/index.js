940244882ab9a4947887978481d21562ab4313ea
Forlocks <volgaopa@gmail.com>
2023-10-22 19:45:03 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,68 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = new Array(expr.length / 10); // Создаём пустой двумерный массив нужной длины
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = [];
+    }
+
+    let dec;
+    for (let i = 0; i < expr.length; i++) { // Записываем в каждый вложенный массив по 10 символов исходной строки
+        if (i % 10 === 0) {
+            dec = i / 10;
+        }
+        arr[dec].push(expr[i]);
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0] === "*") { // Декодируем пробелы
+            arr[i] = [' '];
+        } else {
+            for (let j = 0; j < arr[i].length; j++) { // Избавляемся от лишних нулей в каждом подмассиве c "0" и "1"
+                if (arr[i][j] === '0') {
+                    arr[i].shift();
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            for (let j = 0; j < arr[i].length; j++) { // Превращаем каждый подмассив в последовательность "." и "-"
+                if (arr[i][j] + arr[i][j + 1] === '10') {
+                    arr[i].shift();
+                    arr[i].shift();
+                    arr[i].push('.');
+                    j--;
+                } else if (arr[i][j] + arr[i][j + 1] === '11') {
+                    arr[i].shift();
+                    arr[i].shift();
+                    arr[i].push('-');
+                    j--;
+                }
+            }
+        }
+    }
+
+    let result = [];
+    for (let i = 0; i < arr.length; i++) { // Перемещаем всё из старого массива в новый, где каждый элемент - строка
+        result.push(arr[i].join(''));
+        arr.shift();
+        i--;
+    }
+
+    for (let i = 0; i < result.length; i++) { // Декодируем каждый элемент массива
+        for (let key in MORSE_TABLE) {
+            if (result[i] === key) {
+                result[i] = MORSE_TABLE[key];
+                break;
+            }
+        }
+    }
+
+    result = result.join(''); // Объединяем все декодированные элементы в одну результирующую строку
+
+    console.log(result);
+    return result;
 }
 
 module.exports = {
